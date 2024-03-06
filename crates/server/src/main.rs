3613915e22a4831271452b2bd5a564dd401bf91f
Samuel Craig Carey <sccarey@sandia.gov>
2024-03-06 13:21:09 -0600

    cleanup

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -173,28 +173,3 @@ async fn send(twilio_config: &Configuration, to: String, message: String) -> Res
     trace!("Message sent with SID {}", message.sid.unwrap().unwrap());
     Ok(())
 }
-
-// // Make our own error that wraps `anyhow::Error`.
-// struct AppError(anyhow::Error);
-
-// // Tell axum how to convert `AppError` into a response.
-// impl IntoResponse for AppError {
-//     fn into_response(self) -> Response {
-//         (
-//             StatusCode::INTERNAL_SERVER_ERROR,
-//             format!("Something went wrong: {}", self.0),
-//         )
-//             .into_response()
-//     }
-// }
-
-// // This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
-// // `Result<_, AppError>`. That way you don't need to do that manually.
-// impl<E> From<E> for AppError
-// where
-//     E: Into<anyhow::Error>,
-// {
-//     fn from(err: E) -> Self {
-//         Self(err.into())
-//     }
-// }
